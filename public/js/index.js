c9e5dc0dc5f557f2f50fddeed5f479ad386d5e62
ningxiaoxiao <732869825@qq.com>
2017-10-20 11:13:05 +0800

    直接把参数存进cookie

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 function play() {
     var i = $('#input').val()
-
+    setCookie("addr", i);
 
     //如果是直接给流名字 就补完
     if (i.indexOf("/live/") < 0) {
@@ -19,7 +19,7 @@ function play() {
 
     $('#player').html('<object pluginspage="http://www.macromedia.com/go/getflashplayer" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=10,0,0,0" width="800" height="450" id="ckplayer_a1" name="ckplayer_a1" align="middle"><param name="allowScriptAccess" value="always"><param name="allowFullScreen" value="true"><param name="quality" value="high"><param name="bgcolor" value="#FFF"><param name="wmode" value="transparent"><param name="movie" value="/ckplayer.swf"><param name="flashvars" value="f=rtmp://119.147.180.57/live/cj&amp;c=0&amp;p=1&amp;v=100"><embed allowscriptaccess="always" allowfullscreen="true" quality="high" bgcolor="#FFF" wmode="transparent" src="/ckplayer.swf" flashvars="f=' + i + '&amp;c=0&amp;p=1&amp;v=100" width="800" height="450" name="ckplayer_a1" id="ckplayer_a1" align="middle" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></object>')
 
-    setCookie("addr", i);
+
 }
 
 $(function() {
